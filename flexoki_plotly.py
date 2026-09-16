c1a357bd4214aaa0ce93d5ed8f7bef39be5b6256
Juliano Rosario <jrosario@aptima.com>
2026-09-16 11:16:03 -0700

    fix: update background color for light and dark themes in Flexoki templates

--- flexoki_plotly.py
+++ flexoki_plotly.py
@@ -13,7 +13,7 @@ import plotly.io as pio
 # --- Base values -----------------------------------------------------------
 
 LIGHT = dict(
-    bg="#F0F2FF",
+    bg="#FFFCF0",
     bg2="#F2F0E5",
     ui="#E6E4D9",
     ui2="#DAD8CE",
@@ -24,7 +24,7 @@ LIGHT = dict(
 )
 
 DARK = dict(
-    bg="#F0F2FF",
+    bg="#100F0F",
     bg2="#1C1B1A",
     ui="#282726",
     ui2="#343331",
